31146ca78e7d0c591b38247c72d13ee180ddf30f
VIVEK <99870677+Roninopp@users.noreply.github.com>
2025-10-22 00:08:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,13 @@ load_dotenv()
 
 # Bot Token - Get from @BotFather on Telegram
 # Make sure to put your new revoked token here!
-BOT_TOKEN = "8180268399:AAEfEsmFMvrCsgfxMb5Q0kbPqajrTJbpD38" 
+BOT_TOKEN = "YOUR_NEW_BOT_TOKEN_HERE" 
 
-# --- FIX 3: Added Bot Username and Pic ---
+# --- FIX 3: Added Bot Username ---
 BOT_USERNAME = "Mafia_Gang_Game_Bot" # Change this if your bot's username is different
-MAFIA_PIC_URL = "https://i.imgur.com/83wwv5G.jpeg"
+
+# --- FIX 2: Changed Mafia Pic URL ---
+MAFIA_PIC_URL = "https://i.imgur.com/X25sJtG.jpeg"
 
 
 # Admin user IDs (can manage bot, kick players, etc.)
